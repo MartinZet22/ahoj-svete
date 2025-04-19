cf839a4b773e46561dba3393f5faffb00edd70f3
MartinZet22 <dubencak@gmail.com>
2025-04-19 20:24:51 +0200

    Smazání nápovědy v kódu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     /**
@@ -8,8 +6,7 @@ public class Main {
      */
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Nazdááár!");
         System.out.println("Zkouším první příkaz.");
 
