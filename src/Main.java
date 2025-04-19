fa4e2eb19131a983c8b27b9479bc658a3715cc15
MartinZet22 <dubencak@gmail.com>
2025-04-19 20:17:20 +0200

    Nahrátí celého programu po prvním neúspěšném nahrátí na Github
    Výpis dodatečné hlášky + komentáře
    
    Úvodní seznámení s třídou Main a metodou main().

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    /**
+     * Hlavní metoda programu. Napíšu něco navíc.
+     * @param args Argumenty příkazového řádku.
+     */
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Nazdááár!");
+        System.out.println("Zkouším první příkaz.");
+
+        //Komentář - řádkový
+
+
+        /*
+        * Komentář blokový
+        * Když potřebuji komentář s více řádky
+         */
+
+    }
+}
